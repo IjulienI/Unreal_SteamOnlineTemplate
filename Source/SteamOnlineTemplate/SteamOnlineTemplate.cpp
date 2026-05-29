a424fa92d9eef9786d37deeff0bcea76a6eff44f
IjulienI <89165787+IjulienI@users.noreply.github.com>
2026-05-29 18:14:23 +0200

    Initial Commit

---
+++ Source/SteamOnlineTemplate/SteamOnlineTemplate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SteamOnlineTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SteamOnlineTemplate, "SteamOnlineTemplate" );
